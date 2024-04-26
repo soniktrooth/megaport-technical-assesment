04499dffd9ff050ce662a66e0534184119c06f59
Josh <soniktrooth@gmail.com>
2024-04-26 14:26:43 +1000

    Text change to filters

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -3,10 +3,10 @@ import './filter.css';
 export const Filter = ({ filterText, handleFilter }) => {
   return (
     <div className="filter">
-      <label>Filter</label>
+      <label>Filter deserts</label>
       <input
         type="text"
-        placeholder="Filter"
+        placeholder="Type to filter"
         value={filterText}
         onChange={handleFilter}
       />
